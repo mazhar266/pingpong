29eba72c5bf32144e4caf08e90f0d5068d99e4fe
Mazhar Ahmed <mazhar@seazoobd.com>
2021-07-23 19:39:26 +0600

    completed the service

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
   "fmt"
   "os"
   "log"
-  "time"
-  "context"
+  // "strings"
+  // "time"
+  // "context"
+  "net/http"
   "github.com/joho/godotenv"
   "github.com/influxdata/influxdb-client-go/v2"
 )
@@ -17,25 +19,47 @@ func main() {
     log.Fatal("Error loading .env file")
   }
 
+  // log
+  log.Println("Loaded the .env file")
+
   // get the info from env
   token := os.Getenv("INFLUXDB_TOKEN")
   bucket := os.Getenv("INFLUXDB_BUCKET")
   org := os.Getenv("INFLUXDB_ORG")
   host := os.Getenv("INFLUXDB_HOST")
-  siteurl := os.Getenv("SITE_URL")
+  site_url := os.Getenv("SITE_URL")
+  stat := 1
 
   client := influxdb2.NewClient(host, token)
 
+  // log
+  log.Println("Connected to the influxdb")
+
   // now check if the site is up
+  resp, err := http.Get(site_url)
+  if err != nil {
+    stat = 0
+  } else {
+    defer resp.Body.Close()
+
+    // log
+    log.Println(fmt.Sprintf("Site's status code: %d", resp.StatusCode))
+  }
+
+  // log
+  log.Println("Checked the site-url")
+
+  line := fmt.Sprintf("stat,site=%s up=%d", site_url, stat)
 
   // get non-blocking write client
   writeAPI := client.WriteAPI(org, bucket)
-
   // write line protocol
-  writeAPI.WriteRecord("stat,site=x up=0")
-  writeAPI.WriteRecord("stat,site=x up=1")
+  writeAPI.WriteRecord(line)
   // Flush writes
   writeAPI.Flush()
+
+  // log
+  log.Println("Inserted into influxdb")
 
   // always close client at the end
   defer client.Close()
