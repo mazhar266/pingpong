d973ca6ea5423d0720f2211d21c55616cbe9298a
Mazhar Ahmed <mazhar@seazoobd.com>
2021-07-24 17:59:50 +0600

    fat built the exe files for linux and darwin

--- build-for-linux.sh
+++ build-for-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build . -t pingpong:latest
-docker run -it --rm --volume $PWD:/go/src/pingpong pingpong:latest go build main.go
+docker run -it --rm -e CGO_ENABLED=0 --volume $PWD:/go/src/pingpong pingpong:latest go build -a -v
